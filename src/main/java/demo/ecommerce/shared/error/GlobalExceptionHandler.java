0a56b12adf8843047871493feec4ace09f605961
nguyenbn7 <nguyenbn.email@gmail.com>
2024-06-30 12:45:08 +0700

    rename

--- src/main/java/demo/ecommerce/shared/error/GlobalExceptionHandler.java
+++ src/main/java/demo/ecommerce/shared/error/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @ControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({ NotFoundEntityException.class })
+    @ExceptionHandler({ NotFoundException.class })
     public ResponseEntity<ErrorResponse> handle404(Exception exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(exception.getMessage()));
