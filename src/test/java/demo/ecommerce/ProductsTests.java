0b049a7b470b8fb14ef9b498f2a17d02614aae16
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-03-17 15:55:56 +0700

    add carts test

--- src/test/java/demo/ecommerce/ProductsTests.java
+++ src/test/java/demo/ecommerce/ProductsTests.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.aot.DisabledInAotMode;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
@@ -30,74 +32,75 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @SpringBootTest(classes = { demo.ecommerce.EcommerceApplication.class })
 @AutoConfigureMockMvc
 @Sql(scripts = {
-        "classpath:product_brand_test_data.sql",
-        "classpath:product_test_data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
+		"classpath:product_brand_test_data.sql",
+		"classpath:product_test_data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
 public class ProductsTests {
-    private final long PRODUCT_BRAND_TOTAL_ROWS = 5;
-    private final long PRODUCT_TOTAL_ROWS = 10;
+	private final long PRODUCT_BRAND_TOTAL_ROWS = 5;
+	private final long PRODUCT_TOTAL_ROWS = 10;
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Autowired
-    private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    @Autowired
-    private ProductBrandRepository productBrandRepository;
+	@Autowired
+	private ProductBrandRepository productBrandRepository;
 
-    @Test
-    void checkIfDataLoaded() {
-        assertThat(productBrandRepository.count()).isEqualTo(PRODUCT_BRAND_TOTAL_ROWS);
-        assertThat(productRepository.count()).isEqualTo(PRODUCT_TOTAL_ROWS);
-    }
+	@Test
+	void checkIfDataLoaded() {
+		assertThat(productBrandRepository.count()).isEqualTo(PRODUCT_BRAND_TOTAL_ROWS);
+		assertThat(productRepository.count()).isEqualTo(PRODUCT_TOTAL_ROWS);
+	}
 
-    @Test
-    void get_givenDefault_returnPageProduct() throws Exception {
-        this.mockMvc.perform(get("/v1/products"))
-                .andExpectAll(
-                        status().isOk(),
-                        content().contentType(MediaType.APPLICATION_JSON),
-                        jsonPath("$.pageNumber", is(1)),
-                        jsonPath("$.pageSize", is(5)),
-                        jsonPath("$.totalItems", is(10)),
-                        jsonPath("$.data", hasSize(5)));
-    }
+	@Test
+	void get_givenDefault_returnPageProduct() throws Exception {
+		this.mockMvc.perform(get("/v1/products"))
+				.andExpectAll(
+						status().isOk(),
+						content().contentType(MediaType.APPLICATION_JSON),
+						jsonPath("$.pageNumber", is(1)),
+						jsonPath("$.pageSize", is(5)),
+						jsonPath("$.totalItems", is(10)),
+						jsonPath("$.data", hasSize(5)));
+	}
 
-    @Test
-    void get_givenDefault_returnProductBrands() throws Exception {
-        this.mockMvc.perform(get("/v1/products/brands"))
-                .andExpectAll(
-                        status().isOk(),
-                        content().contentType(MediaType.APPLICATION_JSON),
-                        jsonPath("$", hasSize(5)));
-    }
+	@Test
+	void get_givenDefault_returnProductBrands() throws Exception {
+		this.mockMvc.perform(get("/v1/products/brands"))
+				.andExpectAll(
+						status().isOk(),
+						content().contentType(MediaType.APPLICATION_JSON),
+						jsonPath("$", hasSize(5)));
+	}
 
-    @Test
-    void get_givenBrandId_returnPageProduct() throws Exception {
-        this.mockMvc.perform(get("/v1/products?brandId=" + 5))
-                .andExpectAll(
-                        status().isOk(),
-                        content().contentType(MediaType.APPLICATION_JSON),
-                        jsonPath("$.pageNumber", is(1)),
-                        jsonPath("$.pageSize", is(5)),
-                        jsonPath("$.totalItems", is(1)),
-                        jsonPath("$.data", hasSize(1)));
-    }
+	@Test
+	void get_givenBrandId_returnPageProduct() throws Exception {
+		this.mockMvc.perform(get("/v1/products?brandId=" + 5))
+				.andExpectAll(
+						status().isOk(),
+						content().contentType(MediaType.APPLICATION_JSON),
+						jsonPath("$.pageNumber", is(1)),
+						jsonPath("$.pageSize", is(5)),
+						jsonPath("$.totalItems", is(1)),
+						jsonPath("$.data", hasSize(1)));
+	}
 
-    @Test
-    void get_givenProductIdInData_returnProductDetailWithStatusOk() throws Exception {
-        this.mockMvc.perform(get("/v1/products/1"))
-                .andExpectAll(
-                        status().isOk(),
-                        content().contentType(MediaType.APPLICATION_JSON),
-                        jsonPath("$.name").isString());
-    }
+	@Test
+	void get_givenProductIdInData_returnProductDetailWithStatusOk() throws Exception {
+		this.mockMvc.perform(get("/v1/products/1"))
+				.andExpectAll(
+						status().isOk(),
+						content().contentType(MediaType.APPLICATION_JSON),
+						jsonPath("$.name").isString());
+	}
 
-    @Test
-    void get_givenProductIdNotInData_returnStatusNotFound() throws Exception {
-        this.mockMvc.perform(get("/v1/products/" + (PRODUCT_TOTAL_ROWS + 1)))
-                .andExpectAll(
-                        status().isNotFound(),
-                        content().contentType(MediaType.APPLICATION_JSON));
-    }
+	@Test
+	void get_givenProductIdNotInData_returnStatusNotFound() throws Exception {
+		this.mockMvc.perform(get("/v1/products/" + (PRODUCT_TOTAL_ROWS + 1)))
+				.andExpectAll(
+						status().isNotFound(),
+						content().contentType(MediaType.APPLICATION_JSON));
+	}
 }
