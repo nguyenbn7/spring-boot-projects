ab79d79f7d23b1de243e63b8458e4ce66a765182
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-03-17 15:55:55 +0700

    update test

--- src/main/java/demo/ecommerce/application/products/SeedDemoData.java
+++ src/main/java/demo/ecommerce/application/products/SeedDemoData.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -18,6 +19,7 @@ import demo.ecommerce.application.products.repositories.ProductBrandRepository;
 import demo.ecommerce.application.products.repositories.ProductRepository;
 
 @Configuration
+@Profile("seed")
 public class SeedDemoData {
 
     @Bean
