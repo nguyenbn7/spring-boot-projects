5470a1aa3750e5bf2958ee6b67e2a430b2eb98a4
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-03-17 15:55:53 +0700

    seed product data

--- src/main/java/demo/ecommerce/products/Product.java
+++ src/main/java/demo/ecommerce/products/Product.java
@@ -4,9 +4,12 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -15,7 +18,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
@@ -29,6 +32,13 @@ public class Product {
 
     @Column(nullable = false, length = 555)
     private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "brand_id", insertable = false, updatable = false)
+    private ProductBrand brand;
+
+    @Column(name = "brand_id")
+    private Long brandId;
 
     protected Product() {
     }
--- src/main/java/demo/ecommerce/products/SeedDemoData.java
+++ src/main/java/demo/ecommerce/products/SeedDemoData.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -14,7 +16,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class SeedDemoData {
 
     @Bean
-    CommandLineRunner runner(ProductBrandRepository productBrandRepository, ProductRepository productRepository) {
+    @Order(1)
+    CommandLineRunner seedProductBrand(ProductBrandRepository productBrandRepository) {
         return args -> {
             if (productBrandRepository.count() > 0) {
                 return;
@@ -28,9 +31,32 @@ public class SeedDemoData {
             try {
                 List<ProductBrand> brands = mapper.readValue(inputStream, typeReference);
                 productBrandRepository.saveAll(brands);
-                System.out.println("Product Brands Saved!");
+                System.out.println("Seed product brand data successful");
             } catch (IOException e) {
-                System.out.println("Unable to save Product Brands: " + e.getMessage());
+                System.out.println("Unable to seed product brand data: " + e.getMessage());
+            }
+        };
+    }
+
+    @Bean
+    @Order(2)
+    CommandLineRunner seedProduct(ProductRepository productRepository) {
+        return args -> {
+            if (productRepository.count() > 0) {
+                return;
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<List<Product>> typeReference = new TypeReference<List<Product>>() {
+            };
+            InputStream inputStream = TypeReference.class.getResourceAsStream("/products.json");
+
+            try {
+                List<Product> products = mapper.readValue(inputStream, typeReference);
+                productRepository.saveAll(products);
+                System.out.println("Seed product data successful");
+            } catch (IOException e) {
+                System.out.println("Unable to seed product data: " + e.getMessage());
             }
         };
     }
